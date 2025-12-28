b12cedb20024e71c2013305d7155b2d79d8970d5
sosok-dve <reemreem2023.soso2002@gmail.com>
2025-12-28 15:28:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-/*
-تشغيل وإيقاف الوضع الليلي
-*/
-
+// زر الوضع الليلي
 const btn = document.getElementById("toggle-theme");
 
 btn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-
   btn.textContent = document.body.classList.contains("dark")
     ? "الوضع النهاري"
     : "الوضع الليلي";
 });
+
+// إظهار البطاقات عند النزول
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach((card) => {
+    const cardTop = card.getBoundingClientRect().top;
+
+    if (cardTop < windowHeight - 100) {
+      card.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+// تشغيلها مرة عند الفتح
+revealOnScroll();
